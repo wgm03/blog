205bb16cd3daec0eca2962e74972800f4ceb2291
attract123 <2910698502@qq.com>
2024-09-14 10:59:08 +0800

    Remove all files to prepare for replacement

--- docs/.vuepress/.temp/prismjs/config.js
+++
@@ -1,3 +0,0 @@
-import "F:/markdown/vuepress-site/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
-import "F:/markdown/vuepress-site/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
-import "F:/markdown/vuepress-site/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
